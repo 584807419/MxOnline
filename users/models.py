f77ce05be70f78bf8ed7ec2472d4c574ba948015
584807419 <zk199245ZK>
2018-08-05 16:29:00 +0800

    验证码轮播图

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -7,8 +9,9 @@ from django.contrib.auth.models import AbstractUser
 
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, default="", verbose_name="昵称")
-    birthday = models.CharField(null=True, blank=True, verbose_name="生日")
-    gender = models.CharField(choices=(("male", "男"), ("female", "女")), default="female", verbose_name="性别")
+    birthday = models.DateField(null=True, blank=True, verbose_name="生日")
+    gender = models.CharField(choices=(("male", "男"), ("female", "女")), max_length=5, default="female",
+                              verbose_name="性别")
     address = models.CharField(max_length=100, default="", null=True, blank=True, verbose_name="地址")
     mobile = models.CharField(max_length=11, verbose_name="电话", null=True, blank=True, default="")
     image = models.ImageField(max_length=100, upload_to="image/%Y/%m", default="image/default.jpg", null=True,
@@ -20,3 +23,26 @@ class UserProfile(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name="验证码")
+    email = models.EmailField(max_length=50, verbose_name="邮箱")
+    send_type = models.CharField(choices=(("register", "注册"), ("forget", "找回密码")), max_length=10)
+    send_time = models.DateTimeField(default=datetime.now, verbose_name="发送时间")  # now不加括号(),否则刚开始编译就执行了
+
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name="标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name="轮播图")
+    url = models.URLField(max_length=200, verbose_name="访问地址")
+    index = models.IntegerField(default=100, verbose_name="顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "轮播图"
+        verbose_name_plural = verbose_name
