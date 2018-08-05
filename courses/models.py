065323d47ce35122a8cf478da4b57348577cb347
584807419 <zk199245ZK>
2018-08-05 17:40:02 +0800

    课程信息done

---
+++ courses/models.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+from django.db import models
+
+
+# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=50, verbose_name="课程名称")
+    desc = models.CharField(max_length=300, verbose_name="课程描述")
+    detail = models.TextField(verbose_name="课程详情")
+    degree = models.CharField(choices=((1, "初级"), (2, "中级"), (3, "高级")), verbose_name="课程等级", default=1)
+    learn_times = models.IntegerField(default=0, verbose_name="学习时长(分钟)")
+    students = models.IntegerField(default=0, verbose_name="学生人数")
+    fav_nums = models.IntegerField(default=0, verbose_name="收藏人数")
+    image = models.ImageField(upload_to="courses/%Y/%m", verbose_name="封面图")
+    click_nums = models.IntegerField(default=0, verbose_name="点击量")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程"
+        verbose_name_plural = verbose_name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, verbose_name="课程")
+    name = models.CharField(max_length=100, verbose_name="章节名称")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name="章节"
+        verbose_name_plural = verbose_name
+
+
+class Video(models.Model):
+    lesson = models.ForeignKey(Lesson, verbose_name="章节")
+    name = models.CharField(max_length=100, verbose_name="视频名称")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name="视频"
+        verbose_name_plural = verbose_name
+
+
+class CourseResource(models.Model):
+    course = models.ForeignKey(Course, verbose_name="课程")
+    name = models.CharField(max_length=100, verbose_name="下载资源名称")
+    download = models.FileField(upload_to="course/resource/%Y/%m",verbose_name="资源文件",max_length=100)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name="课程下载资源"
+        verbose_name_plural = verbose_name
