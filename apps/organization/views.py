ce11219962fa7dac4c3df6399e92377eb4362d7b
584807419 <584807419@qq.com>
2018-09-28 15:13:11 +0800

    搞定授课机构筛选

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -12,10 +12,13 @@ class OrgView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
         all_citys = CityDict.objects.all()
+
+        # 取出筛选的城市
         city_id = request.GET.get("city")
         if city_id:
             all_orgs = all_orgs.filter(city_id=int(city_id))
-        category = request.GET.get("ct","")
+        # 类别筛选
+        category = request.GET.get("ct", "")
         if category:
             all_orgs = all_orgs.filter(category=category)
         org_nums = all_orgs.count()
@@ -27,4 +30,8 @@ class OrgView(View):
         p = Paginator(all_orgs, 5, request=request)
         orgs = p.page(page)
         return render(request, "org-list.html",
-                      {"all_orgs": orgs, "all_citys": all_citys, "org_nums": org_nums, "city_id": city_id})
+                      {"all_orgs": orgs,
+                       "all_citys": all_citys,
+                       "org_nums": org_nums,
+                       "city_id": city_id,
+                       "category": category})
