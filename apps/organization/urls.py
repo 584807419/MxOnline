6e0e5994c5fb4b10f994b179466e79f0689a93ad
584807419 <584807419@qq.com>
2018-10-08 09:32:16 +0800

    机构首页开始写啦

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,6 +4,6 @@ from .views import OrgView,AddUserAskView
 urlpatterns = [
     path('list/', OrgView.as_view(), name="org_list"),  # 课程机构首页
     path('add_ask/', AddUserAskView.as_view(), name="add_ask"),  # 提问
-    path('home/(?P<org_id>\d+)', AddUserAskView.as_view(), name="add_ask"),  # 提问
+    path('home/(?P<org_id>\d+)', AddUserAskView.as_view(), name="add_ask"),  # 主页
 
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -67,3 +67,10 @@ class OrgHomeView(View):
     "机构首页"
     def get(self,request,org_id):
         course_org = CourseOrg.objects.get(id=int(org_id))
+        # Course 有 course_org 外键,可以这样取外键对应的所有course
+        all_courses = course_org.course_set.all()[:3]
+        all_teachers = course_org.teacher_set.all()[:1]
+        return render(request,'org-detail-homepage.html',{
+            'all_courses':all_courses,
+            'all_teachers':all_teachers
+        })
