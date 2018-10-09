dd36e19ee793c34e72c0ac3682e72a7c1c890217
584807419 <584807419@qq.com>
2018-10-09 21:19:33 +0800

    课程详情1

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -19,11 +19,19 @@ class Course(models.Model):
     fav_nums = models.IntegerField(default=0, verbose_name="收藏人数")
     image = models.ImageField(upload_to="courses/%Y/%m", verbose_name="封面图")
     click_nums = models.IntegerField(default=0, verbose_name="点击量")
+    category = models.CharField(max_length=20, verbose_name=u"课程类别", default=u"后端开发")
+    tag = models.CharField(max_length=15, verbose_name=u"课程标签", default=u"")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
         verbose_name = "课程"
         verbose_name_plural = verbose_name
+
+    def get_zj_nums(self):
+        return self.lesson_set.all().count()
+
+    def get_learn_users(self):
+        return self.usercourse_set.all()[:5]
 
     def __str__(self):
         return f"{self.name}"
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -3,9 +3,10 @@ from django.views.generic import View
 from django.http import HttpResponse
 
 from .models import Course
-
+from operation.models import UserFavorite, CourseComments, UserCourse
 
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 
 class CourseListView(View):
     @staticmethod
@@ -13,21 +14,21 @@ class CourseListView(View):
         all_courses = Course.objects.all().order_by("-add_time")
         # 热门
         hot_courses = Course.objects.all().order_by("-click_nums")[:3]
-        #排序
-        sort = request.GET.get("sort","")
+        # 排序
+        sort = request.GET.get("sort", "")
         if sort:
             if sort == "students":
                 all_courses = all_courses.order_by("-students")
             elif sort == "hot":
                 all_courses = all_courses.order_by("-click_nums")
 
-        #对课程进行分页
+        # 对课程进行分页
         try:
-            page = request.GET.get("page",1)
+            page = request.GET.get("page", 1)
         except PageNotAnInteger:
             page = 1
 
-        p = Paginator(all_courses,6,request=request)
+        p = Paginator(all_courses, 6, request=request)
         courses = p.page(page)
 
         return render(request, "course-list.html", {
@@ -37,16 +38,35 @@ class CourseListView(View):
             # "search_keywords": search_keywords
         })
 
+
 class CourseDetailView(View):
     @staticmethod
-    def get(request):
-        all_courses = Course.objects.all()
-        return render(request, "course-list.html", {
-            "all_course": all_courses,
-            # "sort": sort,
-            # "hot_courses": hot_courses,
-            # "search_keywords": search_keywords
+    def get(request, course_id):
+        courses = Course.objects.get(id=int(course_id))
+        courses.click_nums += 1
+        courses.save()
+
+        has_fav_course = False
+        has_fav_org = False
+        if request.user.is_authenticated:
+            if UserFavorite.objects.filter(user=request.user, fav_id=courses.id, fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=courses.course_org.id, fav_type=2):
+                has_fav_org = True
+
+        tag = courses.tag
+        if tag:
+            related_course = Course.objects.filter(tag=tag)[1:2]
+        else:
+            related_course = []
+
+        return render(request, "course-detail.html", {
+            "course": courses,
+            "relate_courses": related_course,
+            "has_fav_course": has_fav_course,
+            "has_fav_org": has_fav_org,
         })
+
 
 class CourseInfoView(View):
     @staticmethod
@@ -59,6 +79,7 @@ class CourseInfoView(View):
             # "search_keywords": search_keywords
         })
 
+
 class CommentsView(View):
     @staticmethod
     def get(request):
@@ -70,6 +91,7 @@ class CommentsView(View):
             # "search_keywords": search_keywords
         })
 
+
 class AddCommentsView(View):
     @staticmethod
     def get(request):
@@ -80,6 +102,7 @@ class AddCommentsView(View):
             # "hot_courses": hot_courses,
             # "search_keywords": search_keywords
         })
+
 
 class VideoPlayView(View):
     @staticmethod
