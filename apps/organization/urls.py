ac31cf4712c3cabf26da4ea3a95595c315fd1f0e
584807419 <584807419@qq.com>
2018-10-17 11:37:25 +0800

    讲师详情

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -9,11 +9,9 @@ urlpatterns = [
     re_path('course/(?P<org_id>\d+)/', OrgCourseView.as_view(), name="org_course"),  # 机构课程列表页
     re_path('desc/(?P<org_id>\d+)/', OrgDescView.as_view(), name="org_desc"),  # 机构介绍
     re_path('teacher/(?P<org_id>\d+)/', OrgTeacherView.as_view(), name="org_teacher"),  # 机构讲师介绍
-
     path('add_fav/', AddFavView.as_view(), name="add_fav"),  # 机构收藏
     # 讲师列表
     path('teacher/list/', TeacherListView.as_view(), name="teacher_list"),
-
-    # 访问机构讲师
+    # 讲师详情
     re_path('teacher/detail/(?P<teacher_id>\d+)/', TeacherDetailView.as_view(), name="teacher_detail"),
 ]
